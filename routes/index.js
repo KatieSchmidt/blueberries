ca3fe2702462cb62c9fdfc6014446f83a3ba627b
Katie Schmidt <katie_schmidt7@icloud.com>
2018-07-27 08:33:40 -0400

    customize nav bar color and add images to views

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Four a Day Berries' });
 });
 
+router.get('/berries', (req, res, next) => {
+  res.render('blueberries');
+});
+
+router.get('/bakedGoods', (req, res, next) => {
+  res.render('bakedGoodies');
+});
+
+router.get('/jarsOfYum', (req, res, next) => {
+  res.render('jarsOfYum');
+});
+
+router.get('/baskets', (req, res, next) => {
+  res.render('baskets');
+});
+
 module.exports = router;
